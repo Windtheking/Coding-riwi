1d2e471ffcab45d56f0a5020375cba72aade2b47
Windtheoverlord <santymendoza0106@gmail.com>
2025-07-04 17:33:53 -0500

    Actividad de JS semana1 correcciones 2

--- Modulo 2/Semana 1 JS/project.js
+++ Modulo 2/Semana 1 JS/project.js
@@ -3,13 +3,13 @@
 
     age = parseInt(age);
     if (isNaN(age)){
-        console.log("Insert valid values")
+        document.getElementById("Main_square").innerHTML = "Insert valid values";
     }else{
         if (age <= 0 || age === 0) {
-                console.log("Invalid age");
+                document.getElementById("Main_square").innerHTML = "Invalid age";
         } else if(age >= 18){
             document.getElementById("Main_square").innerHTML = "You are an adult";
         } else if(age < 18 && age > 0){
-            document.getElementById("Main_square").innerHTML = "<div><h1>You are not an adult</h1></div>";
+            document.getElementById("Main_square").innerHTML = "You are not an adult";
         }
     }
