80fe5928041e96569d8aadd190ea190090a4035d
Windtheoverlord <santymendoza0106@gmail.com>
2025-07-03 22:02:58 -0500

    Modificacion absoluta del github, carpetas totales por modulo

---
+++ Modulo 2/Semana 1/Poryect html CV/logic.js
@@ -0,0 +1,15 @@
+
+
+
+/* alert("Page is fully loaded");*/
+
+let first = document.getElementById("submit_button");
+
+document.addEventListener
+
+function dontreload(){
+    let name =  document.getElementById("").value;
+    let email =  document.getElementById("").value;
+}
+
+/*event.preventDefault()  --> para evitar que cuando ocurra algo  se recargue la pagina*/ 
