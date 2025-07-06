c23da0ab9bc6861e1d189b1ed7b85e1034606e36
Windtheoverlord <santymendoza0106@gmail.com>
2025-07-06 15:42:12 -0500

    Now a little bit more of aestetics in js

---
+++ Modulo 2/Semana 1 JS/Java Script html project/project.js
@@ -0,0 +1,15 @@
+    let name = prompt("Insert name ")
+    let age = prompt("Insert age ")
+
+    age = parseInt(age);
+    if (isNaN(age)){
+        document.getElementById("Main_square").innerHTML = `<p>Usser ${name} Inserted invalid value (${age}). Please insert valid value</p>`;
+    }else{
+        if (age <= 0 || age === 0) {
+                document.getElementById("Main_square").innerHTML = `<p>Usser ${name} inserted a Invalid age (${age}). Please ${name} insert a valid age</p>`;
+        } else if(age >= 18){
+            document.getElementById("Main_square").innerHTML = `<p>Usser ${name} You are ${age} years old hence you are an adult</p>`;
+        } else if(age < 18 && age > 0){
+            document.getElementById("Main_square").innerHTML = `<p>Usser ${name} You are ${age} years old hence you are not an adult</p>`;
+        }
+    }
